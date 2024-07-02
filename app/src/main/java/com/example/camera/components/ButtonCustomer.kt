f9c9e04ef3c8b2c51a2d4fd9aef7be3785b7006e
JsMares <JsMares0105@gmail.com>
2024-07-02 00:25:02 -0600

    - Add a customized buttons to save the image

--- app/src/main/java/com/example/camera/components/ButtonCustomer.kt
+++ app/src/main/java/com/example/camera/components/ButtonCustomer.kt
@@ -1,13 +1,12 @@
 package com.example.camera.components
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -15,24 +14,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
-@Preview(showBackground = true)
 @Composable
-fun ButtonCustomer() {
+fun ButtonCustomer(text: String, icon: Int, onClick: () -> Unit) {
     Button(
-        onClick = { },
+        onClick = { onClick() },
         shape = RoundedCornerShape(10f),
         modifier = Modifier
             .padding(12.dp)
             .fillMaxWidth(),
         colors = ButtonDefaults.buttonColors(Color.Black),
+        contentPadding = PaddingValues(vertical = 16.dp)
     ) {
         Row {
-            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "")
+            Icon(painter = painterResource(id = icon), contentDescription = "")
             Spacer(modifier = Modifier.size(16.dp))
-            Text(text = "")
+            Text(text = text)
         }
     }
 }
--- app/src/main/java/com/example/camera/views/HomeView.kt
+++ app/src/main/java/com/example/camera/views/HomeView.kt
@@ -2,6 +2,7 @@ package com.example.camera.views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,15 +12,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.camera.R
+import com.example.camera.components.ButtonCustomer
 
 @Preview(showBackground = true)
 @Composable
 fun HomeView() {
     Box(modifier = Modifier.fillMaxSize()) {
         ImageCustomer(modifier = Modifier.align(Alignment.TopCenter))
+
+        Column(modifier = Modifier.align(Alignment.BottomCenter)) {
+            SaveButtons()
+        }
     }
 }
 
@@ -34,4 +41,20 @@ fun ImageCustomer(modifier: Modifier) {
             .width(250.dp),
         contentScale = ContentScale.Fit
     )
+}
+
+@Composable
+fun SaveButtons() {
+    ButtonCustomer(text = stringResource(id = R.string.save_database), icon = R.drawable.baseline_save_alt_24) {
+
+    }
+    ButtonCustomer(text = stringResource(id = R.string.save_firebase), icon = R.drawable.baseline_local_fire_department_24) {
+
+    }
+    ButtonCustomer(text = stringResource(id = R.string.save_dataRoom), icon = R.drawable.baseline_smartphone_24) {
+
+    }
+    ButtonCustomer(text = stringResource(id = R.string.save_externalMemory), icon = R.drawable.baseline_save_24) {
+
+    }
 }
